6fb1b718ce27536abb662ccd178c503a0f486940
Cody Lindley <contact@codylindley.com>
2015-05-15 14:32:41 -0600

    first commit

---
+++ src/dropdown/dropdown.js
@@ -0,0 +1,20 @@
+//JS
+import kendo from 'kendo-ui';
+//template
+import dropdownTemplate from './dropdownTemplate.html!text';
+//data
+import dropdownData from './dropdownData.json!';
+//css
+import 'kendo-ui/styles/kendo.common-bootstrap.min.css!';
+import 'kendo-ui/styles/kendo.bootstrap.min.css!';
+
+const view = $('<div class="panel panel-default" style="margin:50px"> <div class="panel-heading">Select:</div> <div class="panel-body"><select></select></div></div>').appendTo('body');
+
+const dropdown = new kendo.ui.DropDownList(view.find('select'),{
+    template: kendo.template(dropdownTemplate),
+    dataTextField: 'ContactName',
+    dataValueField: 'CustomerID',
+    dataSource: dropdownData
+});
+
+export {dropdown};
---
+++ src/startup.js
@@ -0,0 +1,7 @@
+import 'bootstrap/css/bootstrap.css!';
+
+System.import('dropdown/dropdown').then(
+	dropdown => console.log(dropdown.dropdown.value()
+));
+
+import _ from 'lodash';
